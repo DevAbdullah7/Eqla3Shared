194c57718caa29c41963262e8d1e17f5457fd94c
ImLegend4658 <MrAziz2005@gmail.com>
2021-11-24 14:41:08 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,33 @@
+<div class="footer">
+    <h6 dir="ltr"><span class='Eng'>Eqla3 Tech</span> <span class="highLine">|</span> إقلاع تيك
+    </h6>
+    <div class="googleAdsense">
+        <a href="#">سياسة الخصوصية</a> <a href="#">اتفاقية الاستخدام</a>
+    </div>
+    <p>Copyright 2021 &copy; All Rights Reserved | <a href="#">Abdullah-Alqahtani.net</a>
+    </p>
+</div>
+</div>
+</div>
+</div>
+<!-- Js Files -->
+<script src="<?php echo $config['app_url'].'/template' ?>/assets/js/main.js"></script>
+<script src="<?php echo $config['app_url'].'/template' ?>/assets/js/typed.js"></script>
+<script>
+    var typed = new Typed('.type', {
+        strings: [
+            'تقنية المعلومات',
+            'قواعد البيانات',
+            'الشبكات',
+            '++c',
+            'نظام Windows',
+            'نظام Linux'
+        ],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true
+    });
+</script>
+</body>
+
+</html>
